6ac9e55e8b7e4cc5eecfd7c486f51ae11349a448
purushottam kumar <puk@growwthpartners.com>
2026-09-02 17:11:24 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
+import type { ViteReactSSGOptions } from 'vite-react-ssg'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+type ViteReactSSGConfig = UserConfig & {
+  ssgOptions?: Partial<ViteReactSSGOptions>
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  ssgOptions: {
+    dirStyle: 'nested',
+  },
+} satisfies ViteReactSSGConfig)
